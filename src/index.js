8ecef090bddd64b4b0207f4dae0b9cf18ea42c6a
Andrei Lukyanau <lukyanoff@tut.by>
2021-08-06 01:03:34 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
 exports.min = function min (array) {
-  return 0;
+	if (Array.isArray(array) && array.length > 1) {
+    	return Math.min(...array);
+    }
+    return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+	if (Array.isArray(array) && array.length > 1) {
+    	return Math.max(...array);
+    }
+    return 0;
 }
 
+
 exports.avg = function avg (array) {
-  return 0;
+	if (Array.isArray(array) && array.length > 1) {
+    	return array.reduce((sum, current) => sum + current, 0)/array.length;
+    }
+    return 0;
 }
